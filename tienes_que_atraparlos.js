e288c80a1e48784e0a060608cfac0419b198f93e
Luciano <91077377+Luciano-C@users.noreply.github.com>
2022-03-05 12:15:49 -0300

    Arreglado error de tipeo

--- tienes_que_atraparlos.js
+++ tienes_que_atraparlos.js
@@ -3,7 +3,7 @@ const prompt = require("prompt-sync")();
 
 let salud_maxima = Number(prompt("Ingrese salud máxima del Pokemón (ST): "));
 let felicidad_pokemon = Number(prompt("Ingrese nivel de felicidad del Pokémon: "));
-let timidez_agresividad = Number(prompt("Ingrese grado de timidez-agresividad del Pokémon: "));;
+let timidez_agresividad = Number(prompt("Ingrese grado de timidez-agresividad del Pokémon(T): "));;
 
 function probabilidad_captura(salud,timidez) {
     let numero_grande = 10**6;
